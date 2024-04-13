3eff4196de1ebcfb1946c79cfb746fb63cc0fac5
Karan <karansuggu@gmail.com>
2024-04-13 16:22:44 +0530

    Created SplashScreen

---
+++ app/src/main/java/com/example/bdwapp/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.bdwapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val intent = Intent(this@SplashScreen,MainActivity::class.java)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(intent)
+        },3000)
+
+    }
+}
