47ae82de1191d4faf955350ebdf58100cbeb73a5
Karan <karansuggu@gmail.com>
2024-04-17 12:57:06 +0530

    firebase authentication

--- app/src/main/java/com/example/bdwapp/SignupActivity.kt
+++ app/src/main/java/com/example/bdwapp/SignupActivity.kt
@@ -3,16 +3,40 @@ package com.example.bdwapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.LinearLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        auth = FirebaseAuth.getInstance()
+
+        val phoneNumber = findViewById<EditText>(R.id.et_number_signup).text.toString()
+
+
         findViewById<LinearLayout>(R.id.ll_to_login).setOnClickListener {
             startActivity(Intent(this@SignupActivity,LoginActivity::class.java))
         }
+
+
+        auth = FirebaseAuth.getInstance()
+
+
+
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
 
 
     }
