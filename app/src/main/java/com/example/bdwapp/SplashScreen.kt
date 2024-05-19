d859b3beaea93ef154abc7efb2e041207aea3329
Karan <karansuggu@gmail.com>
2024-05-20 01:24:33 +0530

    All Todos done.

--- app/src/main/java/com/example/bdwapp/SplashScreen.kt
+++ app/src/main/java/com/example/bdwapp/SplashScreen.kt
@@ -6,15 +6,23 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.example.bdwapp.authentication.SignupActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+    private lateinit var intent: Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        val intent = Intent(this@SplashScreen, SignupActivity::class.java)
+        auth = FirebaseAuth.getInstance()
 
-        // TODO: Navigate directly to DASHBOARD If user is already logged in!
+        if (auth.currentUser != null){
+            intent = Intent(this,MainActivity::class.java)
+        }
+        else{
+            intent = Intent(this@SplashScreen, SignupActivity::class.java)
+        }
 
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
             startActivity(intent)
